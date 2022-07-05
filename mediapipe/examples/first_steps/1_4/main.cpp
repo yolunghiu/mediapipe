4121b6cb56756df6e87a5d8e76a849ad1de81234
liuhy <190978752@qq.com>
2022-07-05 13:53:22 +0800

    1_4 done

--- mediapipe/examples/first_steps/1_4/main.cpp
+++ mediapipe/examples/first_steps/1_4/main.cpp
@@ -1,8 +1,8 @@
 /// Example 1.4 : Calculator options
-/// By Oleksiy Grechnyev, IT-JIM
 /// Here I create a custom calculator with options
 /// It is ideologically simple, but technically tricky
-/// We define a data class GoblinCalculator14Options which is a protobuff class
+/// We define a data class GoblinCalculator14
+/// Options which is a protobuff class
 /// Defined in goblin_calculator14.proto
 /// Then options are supplied in the graph
 /// Note the funny monkey business in BUILD !
@@ -14,13 +14,13 @@
 #include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status.h"
 
-//==============================================================================
-mediapipe::Status run(){
+mediapipe::Status run()
+{
     using namespace std;
     // Create the graph using our custom calculator GoblinCalculator14
     // Note how you supply options to a calculator
-    // Standard MP calculators use options a lot, get used to this somewhat bizarre syntax!
-    // Try to modify the two options!
+    // Standard MP calculators use options a lot, get used to this somewhat
+    // bizarre syntax! Try to modify the two options!
     string protoG = R"(
     input_stream: "in"
     output_stream: "out"
@@ -37,21 +37,23 @@ mediapipe::Status run(){
     }
     )";
 
-    // Let us now parse the string properly this time, checking if the parse is successful
+    // Let us now parse the string properly this time, checking if the parse is
+    // successful
     mediapipe::CalculatorGraphConfig config;
-    if (!mediapipe::ParseTextProto<mediapipe::CalculatorGraphConfig>(protoG, &config)) {
-        // mediapipe::Status is actually absl::Status (at least in the current mediapipe)
-        // So we can create BAD statuses like this
+    if (!mediapipe::ParseTextProto<mediapipe::CalculatorGraphConfig>(protoG,
+                                                                     &config))
+    {
         return absl::InternalError("Cannot parse the graph config !");
-    } 
+    }
 
     // Create MP Graph and intialize it with config
     mediapipe::CalculatorGraph graph;
     MP_RETURN_IF_ERROR(graph.Initialize(config));
 
     // Add observer to "out:
-    auto cb = [](const mediapipe::Packet &packet)->mediapipe::Status{
-        cout << packet.Timestamp() << ": RECEIVED PACKET " << packet.Get<double>() << endl;
+    auto cb = [](const mediapipe::Packet& packet) -> mediapipe::Status {
+        cout << packet.Timestamp() << ": RECEIVED PACKET "
+             << packet.Get<double>() << endl;
         return mediapipe::OkStatus();
     };
     MP_RETURN_IF_ERROR(graph.ObserveOutputStream("out", cb));
@@ -60,9 +62,11 @@ mediapipe::Status run(){
     MP_RETURN_IF_ERROR(graph.StartRun({}));
 
     // Send input packets to the graph, stream "in", then close it
-    for (int i=0; i<13; ++i) {
+    for (int i = 0; i < 13; ++i)
+    {
         mediapipe::Timestamp ts(i);
-        mediapipe::Packet packet = mediapipe::MakePacket<double>(i*0.1).At(ts);
+        mediapipe::Packet packet =
+            mediapipe::MakePacket<double>(i * 0.1).At(ts);
         MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("in", packet));
     }
     graph.CloseInputStream("in");
@@ -72,13 +76,15 @@ mediapipe::Status run(){
     return mediapipe::OkStatus();
 }
 
-//==============================================================================
-int main(){
+int main()
+{
     using namespace std;
     cout << "Example 1.4 : Calculator options" << endl;
+
     // Call run(), which return a status
     mediapipe::Status status = run();
     cout << "status = " << status << endl;
     cout << "status.ok() = " << status.ok() << endl;
+
     return 0;
 }
